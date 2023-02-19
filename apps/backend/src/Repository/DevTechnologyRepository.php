e70150ed710b0d8fc6eec71ccf686c3b233306b7
whitehat33 <g3casas@gmail.com>
2023-02-19 14:12:33 +0100

    Created simple DevTechnologyRepository

---
+++ apps/backend/src/Repository/DevTechnologyRepository.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\DevTechnology;
+
+class DevTechnologyRepository extends DoctrineBaseRepository
+{
+    public function save(DevTechnology $devTechnology) : void
+    {
+        $this->saveEntity($devTechnology);
+    }
+
+    public function remove(DevTechnology $devTechnology) : void
+    {
+        $this->removeEntity($devTechnology);
+    }
+
+    public function findOneById(string $id) : ?DevTechnology
+    {
+        return $this->repository->find($id);
+    }
+
+    protected static function entityClass(): string
+    {
+        return DevTechnology::class;
+    }
+}
